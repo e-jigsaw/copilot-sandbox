c8908b1b02d69e76aae8a57ab33a94cfcd65f25e
jigsaw <m@jgs.me>
2023-07-13 16:49:24 +0900

    impl figlet server

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  fetch() {
+    const body = figlet.textSync("Hello World!");
+    return new Response(body);
+  },
+  port: 3000,
+});
